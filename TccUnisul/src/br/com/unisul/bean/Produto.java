e26e773e3802ae507cc9fedd52aae81b55247c95
jhonatan.martarello@gmail.com <jhonatan.martarello@gmail.com@2dd8ace6-3b72-a03f-b032-802b0c67a029>
2014-04-26 14:26:47 +0000

    Alterações feitas nos Beans.

--- TccUnisul/src/br/com/unisul/bean/Produto.java
+++ TccUnisul/src/br/com/unisul/bean/Produto.java
@@ -1,38 +1,104 @@
 package br.com.unisul.bean;
 
+import java.util.Collection;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
+@Table(name = "produtos")
 public class Produto {
-	@Id @GeneratedValue
-	private Long id;
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	@Column(name = "id")
+	private Long idProduto;
+	@Column(name = "nome")
+	private String nomeProduto;
 	private String descricao;
 	private double preco;
+	
+	@OneToMany(mappedBy = "idProdutoPedido.pedido")
+	private Collection<ProdutoPedido> produtoPedidoList;
 	
 	public Produto(){
 		
 	}
-	
-	public Long getId() {
-		return id;
+
+	public Long getIdProduto() {
+		return idProduto;
 	}
-	public void setId(Long id) {
-		this.id = id;
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
 	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+
+	public Collection<ProdutoPedido> getProdutoPedidoList() {
+		return produtoPedidoList;
+	}
+
+	public void setProdutoPedidoList(Collection<ProdutoPedido> produtoPedidoList) {
+		this.produtoPedidoList = produtoPedidoList;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((idProduto == null) ? 0 : idProduto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (idProduto == null) {
+			if (other.idProduto != null)
+				return false;
+		} else if (!idProduto.equals(other.idProduto))
+			return false;
+		return true;
+	}
+
+	
+
 	
 	
 }
