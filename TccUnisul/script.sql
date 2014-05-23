2effc21969bbe87405087a54d9e58d4d32d9eaf0
jhonatan.martarello@gmail.com <jhonatan.martarello@gmail.com@2dd8ace6-3b72-a03f-b032-802b0c67a029>
2014-05-23 02:08:32 +0000

    Alteração no Script
    SQL pedidos
    
    id_lotePedido -> lotePedido_id

--- TccUnisul/script.sql
+++ TccUnisul/script.sql
@@ -21,7 +21,7 @@ insert into statusPedido (descricao) values ('Cancelado');
 
 
 create table pedidos (id serial primary key, id_cliente int references clientes(id), valorTotalPedido float not null, data date not null, 
-id_statusPedido int references statusPedido(id), observacao varchar(250), retiradoLocal boolean, id_lotePedido int references lotePedido(id));
+id_statusPedido int references statusPedido(id), observacao varchar(250), retiradoLocal boolean, lotePedido_id int references lotePedido(id));
 
 create table produto_pedido (id serial primary key, produto_id int references produtos(id), pedido_id int references pedidos(id), quantidade int);
 
