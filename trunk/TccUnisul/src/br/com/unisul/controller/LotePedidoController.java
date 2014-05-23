4f2a6b53781b8094de2aa0c1d6091a1449eb436d
jhonatan.martarello@gmail.com <jhonatan.martarello@gmail.com@2dd8ace6-3b72-a03f-b032-802b0c67a029>
2014-05-23 02:13:54 +0000

    Nova classe LotePedidoController

---
+++ trunk/TccUnisul/src/br/com/unisul/controller/LotePedidoController.java
@@ -0,0 +1,39 @@
+package br.com.unisul.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.unisul.bean.LotePedido;
+import br.com.unisul.bean.Pedido;
+import br.com.unisul.bean.ProdutoPedido;
+import br.com.unisul.dao.LotePedidoDAO;
+import br.com.unisul.dao.PedidoDAO;
+
+@Controller
+public class LotePedidoController {
+	
+	@RequestMapping(value="adicionarLotePedido", method = RequestMethod.POST)
+	public @ResponseBody String adicionarLotePedido(@RequestBody LotePedido lote) { 
+		
+		new LotePedidoDAO().salvar(lote);			
+
+		return "redirect:index";
+	}
+	
+	
+	@RequestMapping(value = "carregarLoteId/{id}", method= RequestMethod.GET)
+	public @ResponseBody LotePedido getPedido(@PathVariable Long id, Model model) {
+		LotePedido lotePedido = new LotePedidoDAO().buscarLotePedidoPeloId(id);		
+		
+		return lotePedido;
+	}
+}
