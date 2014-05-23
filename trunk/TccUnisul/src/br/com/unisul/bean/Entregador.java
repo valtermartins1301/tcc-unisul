8eba0d2f21261654b95080c68230544f813aca1c
jhonatan.martarello@gmail.com <jhonatan.martarello@gmail.com@2dd8ace6-3b72-a03f-b032-802b0c67a029>
2014-05-23 02:20:20 +0000

    Corrigido relacionamento entre as classes.
    LotePedido
    Pedido
    Entregador

--- trunk/TccUnisul/src/br/com/unisul/bean/Entregador.java
+++ trunk/TccUnisul/src/br/com/unisul/bean/Entregador.java
@@ -1,6 +1,7 @@
 package br.com.unisul.bean;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,6 +16,7 @@ public class Entregador {
 	@Id
 	@GeneratedValue(generator = "increment")
 	@GenericGenerator(name = "increment", strategy = "increment")
+	@Column(name = "id")
 	private Long idEntregador;
 	private String nome;
 	@ManyToOne
