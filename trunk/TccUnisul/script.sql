18b561a67ddc2241fa29b2a4a84182e7df46e765
jhonatan.martarello@gmail.com <jhonatan.martarello@gmail.com@2dd8ace6-3b72-a03f-b032-802b0c67a029>
2014-05-29 21:55:24 +0000

    Alteração no script flagExcluido

--- trunk/TccUnisul/script.sql
+++ trunk/TccUnisul/script.sql
@@ -2,13 +2,15 @@ database name = dinnerChief
 
 create table enderecos (id serial primary key, rua varchar(250), numero varchar(15), complemento varchar(25), bairro varchar(70), cidade varchar(50), cep varchar(15), latitude varchar(50), longitude varchar(50));
 
-create table clientes (id serial primary key, nome varchar(50) not null, telefone varchar(25), email varchar(50), id_endereco int references enderecos(id));
+create table clientes (id serial primary key, nome varchar(50) not null, telefone varchar(25), email varchar(50), flag_excluido boolean, id_endereco int references enderecos(id));
 ALTER TABLE clientes ADD COLUMN flag_excluido boolean;
 ALTER TABLE clientes ALTER COLUMN flag_excluido SET DEFAULT false;
+UPDATE clientes SET flag_excluido = false;
 
-create table produtos (id serial primary key, nome varchar(100) not null, descricao varchar(250), preco float not null);
+create table produtos (id serial primary key, nome varchar(100) not null, descricao varchar(250), preco float not null, flag_excluido boolean);
 ALTER TABLE produtos ADD COLUMN flag_excluido boolean;
 ALTER TABLE produtos ALTER COLUMN flag_excluido SET DEFAULT false;
+UPDATE produtos SET flag_excluido = false;
 insert into produtos (nome, preco, flag_excluido) values ('X-Salada', 8.50, false);
 insert into produtos (nome, preco, flag_excluido) values ('X-Bacon', 8.50, false);
 
@@ -25,7 +27,10 @@ id_statusPedido int references statusPedido(id), observacao varchar(250), retira
 
 create table produto_pedido (id serial primary key, produto_id int references produtos(id), pedido_id int references pedidos(id), quantidade int);
 
-create table entregadores (id serial primary key, nome varchar(50) not null, telefone varchar(25), capacidadeEntrega varchar(50), id_endereco int references enderecos(id));
+create table entregadores (id serial primary key, nome varchar(50) not null, telefone varchar(25), capacidadeEntrega varchar(50), flag_excluido boolean, id_endereco int references enderecos(id));
+ALTER TABLE entregadores ADD COLUMN flag_excluido boolean;
+ALTER TABLE entregadores ALTER COLUMN flag_excluido SET DEFAULT false;
+UPDATE entregadores SET flag_excluido = false;
 
 create table lotePedido (id serial primary key, id_entregador int references entregadores(id));
 
