58e06211aba141616d22ce897d8cdee649dcb77c
jhonatan.martarello@gmail.com <jhonatan.martarello@gmail.com@2dd8ace6-3b72-a03f-b032-802b0c67a029>
2014-05-29 21:48:26 +0000

    Métodos adicionados para manipular objeto Entregador

--- trunk/TccUnisul/src/br/com/unisul/controller/EntregadorController.java
+++ trunk/TccUnisul/src/br/com/unisul/controller/EntregadorController.java
@@ -1,29 +1,67 @@
 package br.com.unisul.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.unisul.bean.Entregador;
-import br.com.unisul.bean.LotePedido;
-import br.com.unisul.bean.Pedido;
+import br.com.unisul.bean.Endereco;
 import br.com.unisul.dao.EntregadorDAO;
-import br.com.unisul.dao.LotePedidoDAO;
 
 @Controller
 public class EntregadorController {
 	
-	@RequestMapping(value="adicionarEntregador", method = RequestMethod.POST)
-	public @ResponseBody String adicionarEntregador(@RequestBody Entregador entregador) { 
+	@RequestMapping("/entregadorListar")
+	public String execute(Model model) {
+		EntregadorDAO dao = new EntregadorDAO();
+		List<Entregador> entregadores = dao.listAll();
+		model.addAttribute("entregadores", entregadores);
+		return "cadastro/entregadorListar";
+	}
+	
+	@RequestMapping("listaEntregadores")
+	public String lista(Model model) {
+		EntregadorDAO dao = new EntregadorDAO();
+		List<Entregador> entregadores = dao.listAll();
+		model.addAttribute("entregadores", entregadores);
+		return "cadastro/entregadorListar";
+	}
+	
+	
+	@RequestMapping(value = "adicionaEditaEntregador", method= RequestMethod.POST)
+	public @ResponseBody String adicionaEditaEntregador(@ModelAttribute(value="entregador") Entregador entregador, @ModelAttribute(value="endereco") Endereco endereco, BindingResult result) {	
+		if(!result.hasErrors()){
+			if(entregador.getIdEntregador() != null){						
+				entregador.setEnderecoEntregador(endereco);
+				new EntregadorDAO().editar(entregador);
+			}else{	
+				entregador.setEnderecoEntregador(endereco);
+				new EntregadorDAO().salvar(entregador);
+			}
+		}
+		return "sucesso";
+	}
+	
+	@RequestMapping(value = "carregarEntregadorId/{id}", method= RequestMethod.GET)
+	public @ResponseBody Entregador getEntregador(@PathVariable Long id, Model model) {
+		Entregador entregador = new EntregadorDAO().buscarEntregadorPeloId(id);
+		return entregador;
+	}
+	
+	@RequestMapping(value="excluirEntregador", method = RequestMethod.POST)
+	public @ResponseBody String excluirCliente(@RequestParam("idEntregador") Long idEntregador) { 
 		
-		new EntregadorDAO().salvar(entregador);
+		new EntregadorDAO().excluir(idEntregador);			
 
-		return "redirect:index";
+		return "sucesso";
 	}
 
 }
