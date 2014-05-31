2c8a36df664e900dd8b0281f9599eafddc799919
ValterMartins.1301@gmail.com <ValterMartins.1301@gmail.com@2dd8ace6-3b72-a03f-b032-802b0c67a029>
2014-05-31 04:48:35 +0000

    Editar lote, adicionar pedidos no lote. Ainda tem problema na hora de salvar lote editado no banco.

--- trunk/TccUnisul/WebContent/WEB-INF/pages/footer.jsp
+++ trunk/TccUnisul/WebContent/WEB-INF/pages/footer.jsp
@@ -7,6 +7,12 @@
 	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false"></script>
 	<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
 
+	<!-- Page-Level Plugin Scripts - Tables -->
+    <script src="bootstrap/js/plugins/dataTables/jquery.dataTables.js"></script>
+    <script src="bootstrap/js/plugins/dataTables/dataTables.bootstrap.js"></script>
+    
+    <!-- SB Admin Scripts - Include with every page -->
+    <script src="bootstrap/js/sb-admin.js"></script>
     	
     <!-- Page-Level Plugin Scripts - Blank -->
     <script src="bootstrap/js/utils/utils.js"></script>
@@ -15,13 +21,6 @@
 	<script src="bootstrap/js/utils/cliente.js"></script>
 	<script src="bootstrap/js/utils/produto.js"></script>
 	<script src="bootstrap/js/utils/entregador.js"></script>
-	
-	<!-- Page-Level Plugin Scripts - Tables -->
-    <script src="bootstrap/js/plugins/dataTables/jquery.dataTables.js"></script>
-    <script src="bootstrap/js/plugins/dataTables/dataTables.bootstrap.js"></script>
-	
-    <!-- SB Admin Scripts - Include with every page -->
-    <script src="bootstrap/js/sb-admin.js"></script>
 
     <!-- Page-Level Demo Scripts - Blank - Use for reference -->
 
--- trunk/TccUnisul/WebContent/bootstrap/js/utils/lotes.js
+++ trunk/TccUnisul/WebContent/bootstrap/js/utils/lotes.js
@@ -21,6 +21,20 @@ $(document).ready(function() {
 			    }
 			}
 		} );
+	
+	$('#lotesModal').on('hide.bs.modal', function() {
+		limparModalLote();
+	});
+	
+	$("#theform").validate({
+		rules: {     
+			idPedido_lotes: {required: true}
+		},
+		tooltip_options: {
+			idPedido_lotes: {trigger:'focus'}
+		},
+	});	
+	
 	} );
 
 //*************************************************************************************
@@ -91,41 +105,64 @@ function criarLote(){
 //*************************************************************************************
 //										Editar Lote
 //*************************************************************************************
-
 function editarLote(id){
 	$.get("carregarLoteId/" + id, function(data){
 		if(data == null){
 			 return;
 		}else{
-			$('#novo_pedido_idLotePedido').val(data.idLotePedido);
+			 
+			$("#modal_lista_lotepedido_idLotePedido").val(id);
+			var listaPedidos = document.getElementById("modal_lista_lotePedidos");	
+			
+			if(data.entregador){
+				$("modal_lista_lotepedido_entregador").val(data.entregador.nomeEntregador);					
+			}	
+				
 			 for(var j=0; j<data.pedidos.length; j++){
 				var pedidos = data.pedidos;
-				var listaPedidos = document.getElementById("modal_lista_lotePedidos");
+				
 				var numeroPedido = pedidos[j].idPedido;
-				var nome = pedidos[j].cliente.nome;
-				var telefone = pedidos[j].cliente.telefone;
-				var entregador = "";
-				if(data.entregador != null){
-					entregador = data.entregador.nomeEntregador;					
-				}			 
+				var cliente 	 = pedidos[j].cliente.nome;
+				var produtos 	 = pedidos[j].produtoPedidoList;
+				var observacoes  = pedidos[j].observacao;
+				var telefone 	 = pedidos[j].cliente.telefone;
+				var data_hora    = pedidos[j].data;
+				
+				
 				var tr = document.createElement("TR");
+				
 				tr.id = numeroPedido;
 				var td = document.createElement("TD");
 				td.innerText = numeroPedido;
+				td.className = "modalLotePedidos_idPedido";
 				tr.appendChild(td);
 				
 				var td = document.createElement("TD");
-				td.innerText = nome;
+				td.innerText = cliente;
 				tr.appendChild(td);
 				
+				var td = document.createElement("TD");
+				td.innerText = "";
+				for(var x=0; x < produtos.length; x++)
+				{
+					if(x!=0)
+						td.innerText += ", ";
+					td.innerText += "("+produtos[x].quantidade+") "+produtos[x].produto.nomeProduto;
+				}
+				tr.appendChild(td);
+				
+				var td = document.createElement("TD");
+				td.innerText = observacoes;
+				tr.appendChild(td);
+								
 				var td = document.createElement("TD");
 				td.innerText = telefone;
 				tr.appendChild(td);
 				
 				var td = document.createElement("TD");
-				td.innerText = entregador;
+				td.innerText = data_hora;
 				tr.appendChild(td);
-									
+													
 				var td = document.createElement("TD");
 				var i = document.createElement("i");
 				i.className = "edit_and_exclude glyphicon glyphicon-remove";
@@ -144,9 +181,71 @@ function editarLote(id){
 }
 
 //*************************************************************************************
+//										Salva Lote Editado
+//*************************************************************************************
+function salvaLoteEditado(){
+
+	var idLotePedido = $('#modal_lista_lotepedido_idLotePedido').val();
+	if(!idLotePedido)
+		return console.log("Erro - salvaLoteEditado() lote sem id!");
+	var pedidosController = new Array();
+	var url = "getPedidos";
+	$.ajax({ 
+		url: url,    
+		type:"GET", 
+		contentType: "application/json; charset=utf-8",
+		async: false,    //Cross-domain requests and dataType: "jsonp" requests do not support synchronous operation
+		cache: false,    //This will force requested pages not to be cached by the browser          
+		processData:false, //To avoid making query String instead of JSON
+		success: function(Resultado) {
+			if(Resultado.status =="ok")
+			{	
+				var dataJson = JSON.parse(Resultado.data);
+				pedidosController = dataJson.listapedidos;
+			}
+		}
+	});
+
+	if(!pedidosController.length)
+	return;
+	
+	var data = $('.modalLotePedidos_idPedido');	
+	var listaPedidos = new Array();
+	
+	for(var i=0; i<data.length ;i++)
+	{
+		var result = $.grep(pedidosController, function(e){ return e.idPedido == data[i].innerText; });
+		if(result == 0) //result[0].statusPedido.descricao != "Pronto para entrega"
+			continue;
+		listaPedidos.push(result[0]);
+	}
+	
+	if(!listaPedidos.length)
+		return;
+	
+	var lotePedido = new Object();
+	lotePedido.idLotePedido = idLotePedido;
+	lotePedido.pedidos 	    = listaPedidos;
+	lotePedido.entregador;
+	
+	var dataJson = JSON.stringify(lotePedido);
+	$.ajax({ 
+		url:"adicionarLotePedido",    
+		type:"POST", 
+		contentType: "application/json; charset=utf-8",
+		data: dataJson, //Stringified Json Object
+		async: false,    //Cross-domain requests and dataType: "jsonp" requests do not support synchronous operation
+		cache: false,    //This will force requested pages not to be cached by the browser          
+		processData:false, //To avoid making query String instead of JSON
+		success: function(resposeJsonObject){
+			alert("Lote criado com sucesso!");
+		}
+	});     	    
+}
+
+//*************************************************************************************
 //									Excluir Lote
 //*************************************************************************************
-
 function excluirLote(id){
 	if (confirm("Realmente deseja excluir?")) {
 		  $.ajax({  
@@ -161,4 +260,90 @@ function excluirLote(id){
 		    }  
 		  });
 		}
+}
+
+
+//****************************************************************************************
+//									Novo Pedido no lote
+//****************************************************************************************
+function adionaPedidoLote() {
+	
+	var id_pedido = document.getElementById("modal_lista_lotepedido_adicionar_pedido_id").value;
+	if(!id_pedido)
+		return;
+	
+	var data = $('.modalLotePedidos_idPedido');	
+	
+	for(var i=0; i<data.length ;i++)
+	{
+		if(idPedido == data[i].innerText)
+			return;
+	}
+	
+	$.get("carregarPedidoId/" + id_pedido, function(pedido) {
+				if (pedido) //&& (!pedido.lotePedido.idLotePedido && pedido.statusPedido.idStatusPedido == 3)
+				{
+					var numeroPedido = pedido.idPedido;
+					var cliente 	  = pedido.cliente.nome;
+					var produtos 	  = pedido.produtoPedidoList;
+					var observacoes  = pedido.observacao;
+					var telefone 	  = pedido.cliente.telefone;
+					var data_hora    = pedido.data;
+					 
+					 var listaPedidos = document.getElementById("modal_lista_lotePedidos");
+						var tr = document.createElement("TR");
+						tr.id = numeroPedido;
+						
+						var td = document.createElement("TD");
+						td.innerText = numeroPedido;
+						tr.appendChild(td);
+						
+						var td = document.createElement("TD");
+						td.innerText = cliente;
+						tr.appendChild(td);
+						
+						var td = document.createElement("TD");
+						td.innerText = "";
+						for(var x=0; x < produtos.length; x++)
+						{
+							if(x!=0)
+								td.innerText += ", ";
+							td.innerText += "("+produtos[x].quantidade+") "+produtos[x].produto.nomeProduto;
+						}
+						tr.appendChild(td);
+						
+						var td = document.createElement("TD");
+						td.innerText = observacoes;
+						tr.appendChild(td);
+										
+						var td = document.createElement("TD");
+						td.innerText = telefone;
+						tr.appendChild(td);
+						
+						var td = document.createElement("TD");
+						td.innerText = data_hora;
+						tr.appendChild(td);
+															
+						var td = document.createElement("TD");
+						var i = document.createElement("i");
+						i.className = "edit_and_exclude glyphicon glyphicon-remove";
+						i.style     = "align=center;";
+						i.onclick = function () {
+						    tr.parentElement.removeChild(tr);
+						};
+						td.appendChild(i);
+						tr.appendChild(td);
+						
+						listaPedidos.appendChild(tr);
+				}
+				else
+					return;
+			});
+}
+
+//**************************************************************************************
+//	Limpar Modal Lotes
+//**************************************************************************************
+function limparModalLote() {
+	document.getElementById("modal_lista_lotePedidos").innerHTML = "";
 }
